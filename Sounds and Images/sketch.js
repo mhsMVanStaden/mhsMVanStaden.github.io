cac93701f7afec17d048cda9252d839f98a6c94b
mhsMVanStaden <48356521+mhsMVanStaden@users.noreply.github.com>
2019-04-23 10:14:43 -0400

    Add files via upload

---
+++ Sounds and Images/sketch.js
@@ -0,0 +1,21 @@
+var mySound; //variable to hold sound
+var img;
+function preload(){
+  //loads sound file variable
+  mySound = loadSound('Mario_Here_We_Go.mp3');
+  img = loadImage('MarioFace.png');
+}
+function setup() {
+  // put setup code here
+  //put canvas for image
+  createCanvas(213,236,)
+  //put variable name in front
+  mySound.setVolume(0.1);
+  mySound.play();
+
+}
+
+function draw(){
+  background(0);
+  image(img,0,0,0,0);
+}
