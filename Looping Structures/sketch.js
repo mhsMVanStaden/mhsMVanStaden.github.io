d9d85207bdad4b3a09e7dbd1ee3ee24fb77c7d44
mhsMVanStaden <48356521+mhsMVanStaden@users.noreply.github.com>
2019-05-08 08:59:20 -0400

    Update sketch.js

--- Looping Structures/sketch.js
+++ Looping Structures/sketch.js
@@ -1,38 +1,44 @@
 function setup() {
   // put setup code here
+  //make a canvas
   createCanvas(1000,600);
 }
 
 function draw() {
+  //create a background
   background(0);
+  //instruct user on how to see the loops in action
   fill(255);
   textFont('Banshee');
   textSize(50);
   text('Click the Mouse to Clone the Circle', 130,100);
-  //create a colour border with rectangles
+  //create a Red border with rectangles
   fill(255,0,0)
   let x1=0;
   while (x1<=1000 ){
-  rect(x1,10,10,10);
-  x1+=10;
+    rect(x1,10,10,10);
+    x1+=10;
   }
+  //create a Green border with rectangles
   fill(0,255,0);
   let x2=0;
   while (x2<=1000 ){
-  rect(x2,580,10,10);
-  x2+=10;
+    rect(x2,580,10,10);
+    x2+=10;
   }
+  //create a Yellow border with rectangles
   fill(255,255,0);
   let y1=0;
   while (y1<=600 ){
-  rect(10,y1,10,10);
-  y1+=10;
+    rect(10,y1,10,10);
+    y1+=10;
   }
+  //create a Blue border with rectangles
   fill(0,0,255);
   let y2=0;
   while (y2<=600 ){
-  rect(980,y2,10,10);
-  y2+=10;
+    rect(980,y2,10,10);
+    y2+=10;
   }
   //if the mouse clicked draw circles from top left to bottom right
   if (mouseIsPressed){
