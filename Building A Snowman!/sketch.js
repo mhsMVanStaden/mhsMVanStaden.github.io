2e7cfe84e21699b820926ac166df556ddaa1063b
mhsMVanStaden <48356521+mhsMVanStaden@users.noreply.github.com>
2019-05-08 08:23:56 -0400

    Update sketch.js

--- Building A Snowman!/sketch.js
+++ Building A Snowman!/sketch.js
@@ -5,21 +5,21 @@ function setup() {
 
 function draw() {
   // put drawing code here
-// The ground
-background(255,255,255);
-fill(0, 255, 0);
-rect(0, 300, 400, 100);
+  // The ground
+  background(255,255,255);
+  fill(0, 255, 0);
+  rect(0, 300, 400, 100);
 
-// The sun
-fill(255,255,0);
-ellipse(80, 64, 100, 100);
+  // The sun
+  fill(255,255,0);
+  ellipse(80, 64, 100, 100);
 
-// The snowman
-fill(255,255,250);
-//Bottom Snowball
-ellipse(200, 300, 150, 150);
-//Middle Snowball
-ellipse(200, 200, 100, 100);
-//Snowman Head
-ellipse(200, 120, 75, 75);
+  // The snowman
+  fill(255,255,250);
+  //Bottom Snowball
+  ellipse(200, 300, 150, 150);
+  //Middle Snowball
+  ellipse(200, 200, 100, 100);
+  //Snowman Head
+  ellipse(200, 120, 75, 75);
 }
